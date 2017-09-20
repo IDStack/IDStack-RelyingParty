a7af4c700e48dafd983f9c029caf4d898c832743
ldclakmal <ldclakmal@gmail.com>
2017-09-20 17:55:26 +0530

    Fix a bug and improvement

--- api/src/main/java/org/idstack/relyingparty/api/APIHandler.java
+++ api/src/main/java/org/idstack/relyingparty/api/APIHandler.java
@@ -23,9 +23,9 @@ public class APIHandler {
     @Autowired
     Router router;
 
-    @RequestMapping("/")
+    @RequestMapping(value = {"/", "/{version}", "/{version}/{apikey}"})
     public void root(HttpServletResponse httpServletResponse) throws IOException {
-        httpServletResponse.sendRedirect("http://idstack.one/relyingparty");
+        httpServletResponse.sendRedirect("http://docs.idstack.apiary.io/");
     }
 
     @RequestMapping(value = "/{version}/{apikey}/saveconfig/basic", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
