2f4f9ad6c1584e3e71ed9aafde947875d6321b42
ldclakmal <ldclakmal@gmail.com>
2017-11-10 10:01:07 +0530

    Remove get cert details api call

--- api/src/main/java/org/idstack/relyingparty/api/controller/APIHandler.java
+++ api/src/main/java/org/idstack/relyingparty/api/controller/APIHandler.java
@@ -220,16 +220,6 @@ public class APIHandler {
         return feature.getDocumentByUrl(storeFilePath, pubFilePath, jsonUrl);
     }
 
-    @RequestMapping(value = "/{version}/{apikey}/getcertdetails", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public String getCertDetails(@PathVariable("version") String version, @PathVariable("apikey") String apikey, @RequestParam(value = "cert_url") String certUrl) {
-        if (!feature.validateRequest(version))
-            return new Gson().toJson(Collections.singletonMap(Constant.Status.STATUS, Constant.Status.ERROR_VERSION));
-        if (!feature.validateRequest(apiKey, apikey))
-            return new Gson().toJson(Collections.singletonMap(Constant.Status.STATUS, Constant.Status.ERROR_API_KEY));
-        return feature.getCertificateDetails(certUrl, tmpFilePath);
-    }
-
     @RequestMapping(value = "/{version}/{apikey}/cleardocstore", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String clearDocStore(@PathVariable("version") String version, @PathVariable("apikey") String apikey) throws IOException {
